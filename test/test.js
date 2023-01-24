5d9a48856cddba7ac5fc10baccebedf44a2df6b9
Martin Heidegger <martin.heidegger@gmail.com>
2023-01-24 15:03:16 +0900

    Adding test to verify #6  - indeed markdown-it doesn't treat markdown code and html coded as mixed.

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const expect = require('chai').expect
 const fs = require('fs')
 const fixtures = {
   env: fs.readFileSync(path.join(__dirname, 'fixtures/env.txt'), 'utf-8'),
+  issue6: fs.readFileSync(path.join(__dirname, 'fixtures/issue6.txt'), 'utf-8'),
   tocPath: path.join(__dirname, 'fixtures/toc.txt')
 }
 const mdReplaceLink = require('../')
@@ -52,6 +53,24 @@ describe('markdown-it-replace-link w. plugin options', function () {
       x: 'test/'
     })
     expect(html).to.equal('<p><a href="test/a">Hello</a></p>\n')
+    done()
+  })
+})
+
+describe('markdown-it-replace-link issue6', function () {
+  const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typography: true
+  }).use(mdReplaceLink, {
+    replaceLink
+  })
+
+  it('works with mixed html code as expected', function (done) {
+    const html = md.render(fixtures.issue6, {
+      x: 'test/'
+    })
+    expect(html).to.equal('<center>![](~/35)\n<center><img src="xxx">\n<p><img src="image/xxx" alt="altPath"></p>\n')
     done()
   })
 })
